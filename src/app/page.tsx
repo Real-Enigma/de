c5c505af8c934145702995ac0ec93bed06abf7db
Anand <anandabraham68@gmail.com>
2023-07-21 19:23:52 +0530

    update : social

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -207,20 +207,28 @@ const Home = () => {
             <li>
             </li>
             <li>
-              <a
-                href="https://www.linkedin.com/in/anand-abraham-98a0b6227/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-black font-semibold hover:text-gray-300 transition-colors duration-300"
-              >
-                LinkedIn
+              <a href="https://www.linkedin.com/in/anand-abraham-98a0b6227" target="_blank" rel="noopener noreferrer">
+                <button type="button" className="text-white bg-[#0077B5] hover:bg-[#0077B5]/90 focus:ring-4 focus:outline-none focus:ring-[#0077B5]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+                  <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path fill="currentColor" d="M416 32H31.9C14.3 32 0 45.8 0 61.6v388.9c0 15.8 14.3 29.6 31.9 29.6H416c17.6 0 32-13.8 32-29.6V61.6C448 45.8 433.6 32 416 32zM135.4 416h-66v-229h66v229zm-33-255c-19.2 0-34.8-15.6-34.8-34.8S83.2 91.4 102.4 91.4 137.2 107 137.2 126.2s-15.6 34.8-34.8 34.8zm285.5 255h-66V290.8c0-28.3-.6-64.8-39.4-64.8-39.5 0-45.6 30.9-45.6 62.9V416h-66v-229h66v33c16.7-25.7 45.5-34.8 70.2-34.8 77.3 0 91.5 50.8 91.5 116.7V416z"></path>
+                  </svg>
+                  LinkedIn
+                </button>
               </a>
+
             </li>
             <li>
-              <button type="button" className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
-                <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>
-                <a href="https://github.com/Real-Enigma" > My Github</a>
-              </button>
+              <a href="https://github.com/YourGitHubUsername" target="_blank" rel="noopener noreferrer">
+                <button type="button" className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2">
+                  <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.207 11.385.6 .113 .793-.262 .793-.577 0-.285-.01-1.04-.015-2.04-3.34 .725-4.042-1.61-4.042-1.61-.546-1.385-1.333-1.754-1.333-1.754-1.092-.745 .083-.73 .083-.73 1.207 .085 1.838 1.236 1.838 1.236 1.07 1.835 2.81 1.305 3.495 .997 .108-.776 .418-1.305 .76-1.605-2.665-.306-5.466-1.335-5.466-5.93 0-1.31 .465-2.38 1.236-3.22-.125-.304-.535-1.527 .117-3.176 0 0 1.008-.322 3.3 1.23 .958-.267 1.983-.398 3.003-.402 1.02 .004 2.045 .135 3.003 .402 2.29-1.552 3.297-1.23 3.297-1.23 .654 1.648 .245 2.872 .12 3.176 .77 .84 1.235 1.91 1.235 3.22 0 4.608-2.805 5.62-5.475 5.92 .42 .372 .81 1.106 .81 2.23 0 1.61-.015 2.91-.015 3.295 0 .317 .192 .69 .8 .572 4.765-1.585 8.2-6.082 8.2-11.385 0-6.627-5.373-12-12-12z"></path>
+                  </svg>
+                  GitHub
+                </button>
+              </a>
+
+
+
             </li>
           </ul>
           <p className="text-sm mt-4">
