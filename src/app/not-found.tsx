be71b320100f0b3adbb6ea3a3091cac9a10ff4b6
Anand <anandabraham68@gmail.com>
2023-07-21 21:36:34 +0530

    fix : error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
       <div className='w-4/5 mx-auto mt-20 flex flex-col justify-center items-center space-y-4'>
         <h1 className='text-4xl font-semibold text-black'>404 - Page Not Found</h1>
         <h4 className='py-10 text-lg text-black font-light'>
-          Sorry, there isn't much to see here. How about you check out the rest of our amazing website?
+          Sorry, there is not much to see here. How about you check out the rest of our amazing website?
         </h4>
        
         <div className='space-x-4 justify-center'>
